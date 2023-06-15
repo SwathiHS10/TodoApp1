1d374407369d9e6221e8fac85d8d2e61bbe70638
SwathiHS10 <swathihs26@gmail.com>
2023-06-15 14:26:19 +0530

    Add project files.

---
+++ TodoApp1/Models/UserLogins.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TodoApp1.Models
+{
+    public class UserLogins
+    {
+        [Required]
+        public string UserName
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+        public UserLogins() { }
+    }
+}
